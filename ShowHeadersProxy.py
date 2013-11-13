645ed5d708f32f40c8c1904691fd41e75e3afe4a
inaz2 <inaz2@twitter>
2013-11-13 12:42:08 +0900

    ShowHeadersProxy.py: use NEL control character for line break (useful for grep)

--- ShowHeadersProxy.py
+++ ShowHeadersProxy.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 
 from SimpleHTTPProxy import SimpleHTTPProxyHandler, test
+import sys
 
 class ShowHeadersProxyHandler(SimpleHTTPProxyHandler):
     version_table = {10: 'HTTP/1.0', 11: 'HTTP/1.1', 9: 'HTTP/0.9'}
 
     def save_handler(self, req, res, body):
-        print '----'
-        print req.requestline
-        print req.headers
-        print self.version_table[res.version], res.status, res.reason
-        print res.headers
+        text = """----
+%s
+%s
+%s %d %s
+%s
+""" % (req.requestline, str(req.headers), self.version_table[res.version], res.status, res.reason, str(res.headers))
+        print text.replace('\n', '\x1bE')    # use NEL control character for line break (useful for grep)
+        sys.stdout.flush()
 
 
 if __name__ == '__main__':
