5ec903c5cbd39b75014416441a3e774326e31df0
inaz2 <inaz2@twitter>
2013-11-21 22:11:35 +0900

    StripAmazonProxy.py: fix regexp

--- StripAmazonProxy.py
+++ StripAmazonProxy.py
@@ -5,10 +5,10 @@ import re
 
 class StripAmazonProxyHandler(SimpleHTTPProxyHandler):
     def request_handler(self, req, reqbody):
-        m = re.match(r'http://www\.amazon\.co\.jp/.+?/dp/([A-Z0-9]{10})', req.path)
+        m = re.match(r'http://www\.amazon\.co\.jp/[^?]+?/dp/([A-Z0-9]{10})', req.path)
         if m:
             redirect_url = "http://www.amazon.co.jp/dp/%s" % m.group(1)
-            if req.path == redirect_url:
+            if redirect_url == req.path:
                 return
 
             self.send_response(302)
